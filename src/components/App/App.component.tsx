9ccbb9ac069d9db6b3ef2467997109ffabe83c2b
wveenendaal <wveenendaal@quintor.nl>
2019-09-27 08:39:17 +0200

    - changed css to scss - moved App to a separate 'module' folder

---
+++ src/components/App/App.component.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.scss';
+import Content from "../Content";
+
+const App: React.FC = () => (
+    <div className='App'>
+        <Content/>
+    </div>
+);
+
+export default App;
