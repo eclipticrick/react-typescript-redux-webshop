3bfedaeda142c7a211f96cd7871c9beccd805845
wveenendaal <wveenendaal@quintor.nl>
2019-09-28 18:11:19 +0200

    - updated App.component to use a Router and the redux store

--- src/components/App/App.component.tsx
+++ src/components/App/App.component.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.scss';
-import Content from "../Content";
+import ContentComponent from "../Content";
+import { BrowserRouter as Router } from "react-router-dom";
+import NavigationBarComponent from "../NavigationBar";
+import {Provider} from "react-redux";
+import store from "../../store";
+import {fetchProducts} from "../../store/app/store";
 
-const App: React.FC = () => (
-    <div className='App'>
-        <Content/>
-    </div>
-);
+const App: React.FC = () => {
+    useEffect(() => {
+        store.dispatch(fetchProducts());
+    }, []);
+    return (
+        <Provider store={store}>
+            <Router>
+                <div className='App theme-green'>
+                    <NavigationBarComponent/>
+                    <ContentComponent/>
+                </div>
+            </Router>
+        </Provider>
+    )
+};
 
 export default App;
